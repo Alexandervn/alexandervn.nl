965eed8dea19d1bbca60e3330755c4d2e32b13da
Alexander van Noord <alexander@therock.nl>
2015-05-10 19:31:16 +0200

    Update bucket and region

--- push.sh
+++ push.sh
@@ -14,7 +14,7 @@ find build/ -iname '*.gz' -exec rename 's/\.gz$//i' {} +
 echo '- S3'
 
 # Text-like files
-aws s3 sync build/ s3://blog.alexandervn.nl --acl public-read --content-encoding 'gzip' --exclude '*.*' --include '*.html' --include '*.css' --include '*.js' --include '*.svg' --include '*.txt' --delete --region eu-west-1
+aws s3 sync build/ s3://alexandervn.nl --acl public-read --content-encoding 'gzip' --exclude '*.*' --include '*.html' --include '*.css' --include '*.js' --include '*.svg' --include '*.txt' --delete --region eu-central-1
 
 # Media-like files
-aws s3 sync build/ s3://blog.alexandervn.nl --acl public-read --exclude '*.*' --include '*.pdf' --include '*.dot' --include '*.jpg' --include '*.png' --delete --region eu-west-1
+aws s3 sync build/ s3://alexandervn.nl --acl public-read --exclude '*.*' --include '*.pdf' --include '*.dot' --include '*.jpg' --include '*.png' --delete --region eu-central-1
